15888ae28aeb0e6caf827ec774f7d387efc68ceb
erayalkis <erayalkis@hotmail.com>
2022-05-18 17:20:57 +0300

    Run migration AddIndexToUserDetails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_135848) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_141947) do
   create_table "commissions", force: :cascade do |t|
     t.integer "commissioner_id"
     t.integer "artist_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_135848) do
     t.string "user_personal_site_url", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_details_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
