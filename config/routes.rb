b3b8da98bcad211eb0daa98dce1b48c3563fc241
erayalkis <erayalkis@hotmail.com>
2022-05-18 20:09:30 +0300

    Create routes for Commissions

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :posts, only: [:index, :show, :create, :update, :destroy]
+  resources :posts,       only: [:index, :show, :create, :update, :destroy]
+  resources :commissions, only: [:index, :show, :create, :update, :destroy]
 end
