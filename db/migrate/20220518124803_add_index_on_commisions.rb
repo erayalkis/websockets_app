e961595731450808a6f50d507e46952889250b93
erayalkis <erayalkis@hotmail.com>
2022-05-18 15:49:26 +0300

    Make previously generated index unique

--- db/migrate/20220518124803_add_index_on_commisions.rb
+++ db/migrate/20220518124803_add_index_on_commisions.rb
@@ -1,5 +1,5 @@
 class AddIndexOnCommisions < ActiveRecord::Migration[7.0]
   def change
-    add_index :commisions, [:commisioner_id, :artist_id]
+    add_index :commisions, [:commisioner_id, :artist_id], unique: true
   end
 end
