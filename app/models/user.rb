4946e351d95a575d6bce2574ae0d3201eedd5b19
erayalkis <erayalkis@hotmail.com>
2022-05-18 17:59:04 +0300

    Create associations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one :user_details
+  has_many :posts
+  has_many :commisions, primary_key: 'artist_id'       # Received comms
+  has_many :requests,   primary_key: 'commissioner_id' # Sent comms
 end
