6a5f8827130ebc142c8bb37e3b2ab5bc4ceea731
erayalkis <erayalkis@hotmail.com>
2022-05-18 20:08:50 +0300

    Write code for CommissionsController

--- app/controllers/commissions_controller.rb
+++ app/controllers/commissions_controller.rb
@@ -1,2 +1,43 @@
 class CommissionsController < ApplicationController
+
+  def index
+    @commissions = Commission.all
+
+    if @commissions
+      render json: @commissions
+    else
+      render json: { error: 'Commissions not found!' }, status: 404
+    end
+  end
+
+  def show
+    @commission = Commission.find(params[:id])
+
+    if @commission
+      render json: @commission
+    else
+      render json: { error: 'Commission not found!' }, status: 404
+    end
+  end
+
+  def update
+    @commission = Commission.find(params[:id])
+
+    if @commission.update!(commission_params)
+      render json: @commission
+    else
+      render json: @commission.errors.full_messages
+    end
+  end
+
+  def destroy
+    @commission = Commission.find(params[:id])
+    @commission.destroy
+  end
+
+  private
+
+  def commission_params
+    params.require(:commission).permit(:artist_id, :commissioner_id)
+  end
 end
