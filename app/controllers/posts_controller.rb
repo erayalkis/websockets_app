70f72b50219ed30889bb5f5c2be2d461e6c6ff65
erayalkis <erayalkis@hotmail.com>
2022-05-18 19:26:38 +0300

    Add code to posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,53 @@
 class PostsController < ApplicationController
+  def index
+    @post = Post.all
+
+    if @post
+      render json: @post
+    else
+      render json: { msg: 'Posts not found!' }, status: 404
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+
+    if @post
+      render json: @post
+    else
+      render json: { msg: 'Post not found!' }, status: 404
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.create!
+      render json: @post
+    else
+      render json: @post.errors.full_messages, status: 500
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if post.update!(post_params)
+      render json: @post
+    else
+      render json: @post.errors.full_messages, status: 500
+    end
+  end
+
+  def destroy
+   @post = Post.find(params[:id])
+   @post.destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id)
+  end
+
 end
